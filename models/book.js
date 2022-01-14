42f258689243641d59584b1613c3df8a61637e2e
robert604 <robtemper@gmail.com>
2022-01-14 14:14:46 -0500

    submission for 8.15

--- models/book.js
+++ models/book.js
@@ -25,6 +25,7 @@ schema.set('toJSON',{
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
+    returnedObject.author = returnedObject.author.name
   }
 })
 
